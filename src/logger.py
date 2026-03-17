7beff345fd88b1da692702c4297d30ae34cb2f43
Omriberchman <oberechman@gmail.com>
2026-03-17 14:40:45 +0200

    Logger and display funcional.

--- src/logger.py
+++ src/logger.py
@@ -1,30 +1,45 @@
 import csv
-import collector
+import time
 
-information= {"Disk":collector.getDisk()}
+information = {}
+stats = []
 def init(passed_info):
     global information
     information = passed_info
+    global stats
+    stats = [information['CPU'][0], information['CPU'][1],round(information['RAM'][0]/10**9,2),information['RAM'][1],round(information['RAM'][2]/10**9,2)]
 
+    columns = ["CPUWhole", "CPUAllCores", "RAMTotal", "RAMPercentage", "RAMUsed"] 
+    #Add partition columns:
+    for key, value in information['DISK'].items():
+        key = key[:1] #"C:/" == > "C" since partitions can only have 1 letter no issue..
+        columns.append(str(key)+"Total")
+        columns.append(str(key)+"Used")
+        columns.append(str(key)+"Percent")
+    #Add time parition:
+    columns.append("Time")
+    with open('log.csv','r') as log_file: #Checking if the file is empty (to add the first row as "title") or to skip that.
+        if log_file.read(1): #file is empty
+            pass 
+        else: #file already has titles
+            with open('log.csv', 'w', newline='') as log_file: 
+                writer = csv.writer(log_file)
+                writer.writerow(columns)
 
-columns = ["CPUWhole", "CPUAllCores", "RAMTotal", "RAMPercentage", "RAMUsed"] 
-#Add partition columns:
-for key, value in information['Disk'].items():
-    key = key[:1] #"C:/" == > "C" since partitions can only have 1 letter no issue..
-    columns.append(str(key)+"Total")
-    columns.append(str(key)+"Used")
-    columns.append(str(key)+"Percent")
+    for key in information['DISK'].values(): 
+        stats.append(int(key[0]/10**9))
+        stats.append(int(key[1]/10**9))
+        stats.append(key[3])
+    stats.append(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
     
 
-print(columns)
-with open('log.csv', mode='w') as log_file:
-    writer = csv.writer(log_file)
-    writer.writerow(columns)
+def printToLog():
+    global stats
+    with open('log.csv','a', newline='') as log_file: #a for append
+        writer = csv.writer(log_file, )
+        writer.writerow(stats)
 
+def NukeFile(): #clear the log file
+    with open("log.csv", "w") as f:
+        pass
 
-# Not writing anything just yet, consider using this...
-
-# information = {"CPU":getCpuPercent(),"RAM":getRAM(),"DISK":getDisk()}
-# stats = [information['CPU'][0], information['CPU'][1],information['RAM'][0],information['RAM'][1],information['RAM'][2]]
-# print(information)
-# print(stats)
