f50025124faac8485744a8372392eec10faa4627
Omriberchman <oberechman@gmail.com>
2026-03-19 16:54:39 +0200

    continued work on edge cases for logger

--- src/collector.py
+++ src/collector.py
@@ -1,20 +1,20 @@
-import psutil
+from psutil import cpu_percent, virtual_memory, disk_partitions,disk_usage
 
 
 def getCpuPercent():
-    stats = psutil.cpu_percent(interval=0.5,percpu=True) #returns the full list of each CPU/core (in my case, 20)
+    stats = cpu_percent(interval=0.5,percpu=True) #returns the full list of each CPU/core (in my case, 20)
     whole = str(int(sum(stats)/len(stats))) #returns the average (like the windows task manager)
     return [str(int(whole)),stats]
 
 def getRAM():
-    return [(psutil.virtual_memory())[i] for i in (0,2,3)] #returns total, percentage and used in a list. Note it includes the pagefile (swap), for me it's 1.1GB thus upping the total..
+    return [(virtual_memory())[i] for i in (0,2,3)] #returns total, percentage and used in a list. Note it includes the pagefile (swap), for me it's 1.1GB thus upping the total..
 
 def getDisk():
-    partitions_list = psutil.disk_partitions() 
+    partitions_list = disk_partitions() 
     partitions = [partition[1] for partition in partitions_list] #The list of mounted partition letters [C:\,D:\...]."running over" the former partitions to save on creating new variables.
     results = {}
     for i in range(len(partitions)):
-        results[partitions_list[i][0]] = psutil.disk_usage(partitions[i])
+        results[partitions_list[i][0]] = disk_usage(partitions[i])
     
     return results
 
--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import time
 
 information = {}
 stats = []
-num_of_columns = 0 
+
 
 def init(passed_info):
     global information
@@ -21,7 +21,6 @@ def init(passed_info):
     #Add time parition:
     columns.append("Time")
     global num_of_columns
-    num_of_columns = len(columns)
 
 
     try:
@@ -41,18 +40,24 @@ def init(passed_info):
         stats.append(int(key[1]/10**9))
         stats.append(key[3])
     stats.append(str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-    
+
+
+def numOfColumns():
+    with open('log.csv',"r") as log_file:
+        number_of_words = log_file.readline().count(",")    
+        return number_of_words+1 #Because the end of the line is an invisible char which is not typable
+
 
 def printToLog():
     global stats
-    global columns
-    if (len(stats) != num_of_columns): #not len(columns) because at time of declaration (if I declate outside of the init func) it only has the CPU and RAM ones.
+    print(f"~~~ {len(stats)} = {numOfColumns()} ~~~")
+    if (len(stats) != numOfColumns()): #num_of_columns and not len(columns) because at time of declaration (if I declate outside of the init func) it only has the CPU and RAM ones.
         print("### OOPS NUMBER OF PARTIOTIONS CHANGED! ###")
     with open('log.csv','a', newline='') as log_file: #a for append
         writer = csv.writer(log_file, )
         writer.writerow(stats)
 
+
 def NukeFile(): #clear the log file
     with open("log.csv", "w") as f:
         pass
-
